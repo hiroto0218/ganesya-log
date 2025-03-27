24a81653692a3fd15ace4b4ad845fb485e22e328
hiroto0218 <1610hf@gmail.com>
2025-03-28 00:09:59 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-// 読み込み不具合修正版 script.js
+console.log('script.js 正常に読み込まれました（テスト用）');
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("weekly-task-title").textContent = "（例）靴を磨く";
+  document.getElementById("daily-advice").textContent = "お気に入りの靴を1足選んで磨いてみよう。";
+});
